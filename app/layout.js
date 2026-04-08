246f9b5e467b139643340ae279b91b432ef3567f
Khushal-Nikhare <kushalnikhare@gmail.com>
2026-04-08 23:27:38 +0530

    feat: implement root layout with SEO metadata, Google Analytics, and theme initialization

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WHRZX3L2ND"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WHRZX3L2ND');
+            `,
+          }}
+        />
         <script
           dangerouslySetInnerHTML={{
             __html:
